6bae1ec13b420b47bb0a7201d239b872c1b2dc5b
Saurav <saurav.win.m@gmail.com>
2025-03-12 16:44:53 +0100

    Added about GameDev

--- GitConcepts.py
+++ GitConcepts.py
@@ -1,7 +1,12 @@
 def printMaths():
     print("I like Maths")
 
+def printGame():
+    print("I enjoy game development")
+
 
 # Just prints
 print("I learnt Git Concepts")
+
 printMaths()
+printGame()
